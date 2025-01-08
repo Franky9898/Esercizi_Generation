abc1ad5c9199cb6b2bec1b8c327eeaf15de312b8
Francesco <martinifranky98@gmail.com>
2025-01-08 14:03:51 +0100

    modificato main un po'

--- Esercizi_post_vacanze/src/bingo/Main.java
+++ Esercizi_post_vacanze/src/bingo/Main.java
@@ -11,20 +11,16 @@ public class Main
 		boolean cinquina = false;
 		boolean tombola = false;
 		Cartella.stampaCartella(cartella);
-		boolean soloUno = true; // Usato per stampare CINQUINA solo una volta
 		int indiceEstrazione = 0;
 		while (!(cinquina && tombola))
 		{
 			int estratto = Tabellone.estrazioneNumeri(tabellone, indiceEstrazione);
 			Cartella.estratto(cartella, estratto);
-			if (!cinquina) // Si controlla la cinquina fino a quando non si fa
-			{
-				cinquina = Cartella.cinquina(cartella);
-			}
-			if (cinquina && soloUno)
-			{
-				System.err.println("CINQUINA!");
-				soloUno = false;
+			if (!cinquina) {
+			    cinquina = Cartella.cinquina(cartella);
+			    if (cinquina) {
+			        System.err.println("CINQUINA!");
+			    }
 			}
 			if (cinquina) // È inutile controllare se si è fatto tombola se non si è fatta la cinquina
 			{
