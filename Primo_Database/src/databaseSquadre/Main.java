e5eff4a227a3a64be5db39caeae36a292aff3fdd
Francesco <martinifranky98@gmail.com>
2025-01-08 12:54:18 +0100

    Aggiunti commenti

--- Primo_Database/src/databaseSquadre/Main.java
+++ Primo_Database/src/databaseSquadre/Main.java
@@ -11,25 +11,27 @@ public class Main
 	public static void main(String[] args)
 	{
 		ricercaGiocatori(25, 29);
+		System.out.println("");
 		ricercaGoal(30);
+		System.out.println("");
 		ricercaNome("M%");
 	}
-
-	public static void ricercaGiocatori(int eta1, int eta2)
+	// Primo Passo: scrivere query (in realtà anche dopo)
+	// Secondo Passo: Inizializzare connessione con Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)
+	// Terzo Passo: Preparare lo statement (meglio se di tipo prepareStatement per evitare SQLInjection) con PreparedStatement pstmt = conn.prepareStatement(query)
+	// Quarto Passo: Gestire il result con ResultSet rs = pstmt.executeQuery()
+	// Quinto Passo: Probabilmente chiudere le connessioni
+	
+	public static void ricercaGiocatori(int eta1, int eta2) // Ricerca per età compresa tra eta1 e eta2
 	{
 		String query = "SELECT * FROM gestione_squadre.calciatore WHERE eta BETWEEN ? AND ?;";
 
 		try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD); PreparedStatement pstmt = conn.prepareStatement(query))
 		{
-
-			// Associo al placeholder ? (il primo e unico in questo caso) la stringa brand
 			pstmt.setInt(1, eta1);
 			pstmt.setInt(2, eta2);
-
-			// Eseguo la query e ottengo il ResultSet
 			try (ResultSet rs = pstmt.executeQuery())
 			{
-				// Ciclo sui risultati
 				while (rs.next())
 				{
 					int id = rs.getInt("idCalciatore");
@@ -37,18 +39,16 @@ public class Main
 					int etaGioc = rs.getInt("eta");
 					int goal = rs.getInt("goal");
 					int idSquadra = rs.getInt("idSquadra");
-
 					System.out.printf("ID: %d | Nome: %s | Eta: %d | Goal: %d | idSquadra: %d%n", id, nome, etaGioc, goal, idSquadra);
 				}
 			}
-
 		} catch (SQLException e)
 		{
 			e.printStackTrace();
 		}
 	}
 
-	public static void ricercaGoal(int goalGiocatore)
+	public static void ricercaGoal(int goalGiocatore) // Ricerca per goal superiori a goalGiocatore
 	{
 		String query = "SELECT calciatore, goal FROM calciatore WHERE goal >= ?";
 		try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD); PreparedStatement pStatement = connection.prepareStatement(query))
@@ -56,27 +56,28 @@ public class Main
 			pStatement.setInt(1, goalGiocatore);
 			try (ResultSet result = pStatement.executeQuery())
 			{
-				while(result.next()) 
+				while (result.next())
 				{
 					String nome = result.getString("calciatore");
 					int goal = result.getInt("goal");
 					System.out.printf("Nome: %s, Goal: %d%n", nome, goal);
 				}
 			}
-		} catch (SQLException e) 
+		} catch (SQLException e)
 		{
 			e.printStackTrace();
 		}
 	}
-	public static void ricercaNome(String iniziale) 
+
+	public static void ricercaNome(String iniziale) // Ricerca per nome che inizia con iniziale
 	{
 		String query = "SELECT calciatore, eta, idSquadra FROM calciatore WHERE calciatore LIKE ? ";
-		try(Connection conn2 = DriverManager.getConnection(URL, USER, PASSWORD); PreparedStatement pStat = conn2.prepareStatement(query))
+		try (Connection conn2 = DriverManager.getConnection(URL, USER, PASSWORD); PreparedStatement pStat = conn2.prepareStatement(query))
 		{
-			pStat.setString(1,iniziale);
-			try (ResultSet res = pStat.executeQuery()) 
+			pStat.setString(1, iniziale);
+			try (ResultSet res = pStat.executeQuery())
 			{
-				while(res.next()) 
+				while (res.next())
 				{
 					String nome = res.getString("calciatore");
 					int eta = res.getInt("eta");
@@ -84,10 +85,9 @@ public class Main
 					System.out.printf("Nome: %s | Eta: %d | Squadra:%d%n", nome, eta, squadra);
 				}
 			}
-		}
-		catch(SQLException e) {
+		} catch (SQLException e)
+		{
 			e.printStackTrace();
 		}
 	}
-
 }
