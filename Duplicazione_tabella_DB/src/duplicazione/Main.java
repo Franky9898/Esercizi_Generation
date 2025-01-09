22a80e78c89c85e7a731f6de2900744d734bd57b
Francesco <martinifranky98@gmail.com>
2025-01-09 19:04:31 +0100

    Aggiuto try-with-resources per result set

--- Duplicazione_tabella_DB/src/duplicazione/Main.java
+++ Duplicazione_tabella_DB/src/duplicazione/Main.java
@@ -16,29 +16,34 @@ public class Main
 		duplicazioneDati("esercizi_generation2.libri", libreria);
 	}
 
-	/* Metodo per selezionare tutti i campi della tabella libri
-	 * @param nomeTabella:  Nome della tabella con relativo database  
+	/*
+	 * Metodo per selezionare tutti i campi della tabella libri
+	 * 
+	 * @param nomeTabella: Nome della tabella con relativo database
+	 * 
 	 * @return ArrayList: Contiene tutti gli oggetti di tipo libro trovati nella tabella
 	 */
-	
+
 	public static ArrayList<Libro> selezioneDati(String nomeTabella)
 	{
 		ArrayList<Libro> libreria = new ArrayList<Libro>();
-		String querySelezione = "SELECT * FROM " + nomeTabella; 
+		String querySelezione = "SELECT * FROM " + nomeTabella;
 		try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD); Statement stmt = conn.createStatement())
 		{
-			ResultSet resultSet = stmt.executeQuery(querySelezione);
-			while (resultSet.next()) // Si parte dall'indice 2 perchè non si vuole maneggiare l'ID della tabella originale
+			try (ResultSet resultSet = stmt.executeQuery(querySelezione))
 			{
-				String autore = resultSet.getString(2); 
-				String titolo = resultSet.getString(3);
-				int annoPubblicazione = resultSet.getInt(4);
-				double prezzo = resultSet.getDouble(5);
-				Libro libro = new Libro(autore, titolo, annoPubblicazione, prezzo); // Creazione oggetto libro con attributi ricavati dalla tabella
-				libreria.add(libro);
+				while (resultSet.next()) // Si parte dall'indice 2 perchè non si vuole maneggiare l'ID della tabella originale
+				{
+					String autore = resultSet.getString(2);
+					String titolo = resultSet.getString(3);
+					int annoPubblicazione = resultSet.getInt(4);
+					double prezzo = resultSet.getDouble(5);
+					Libro libro = new Libro(autore, titolo, annoPubblicazione, prezzo); // Creazione oggetto libro con attributi ricavati dalla tabella
+					libreria.add(libro);
+				}
+				System.out.println("Riuscito"); // Per capire se funziona
+				return libreria;
 			}
-			System.out.println("Riuscito"); //Per capire se funziona
-			return libreria;
 		} catch (SQLException e)
 		{
 			e.printStackTrace();
@@ -46,8 +51,12 @@ public class Main
 		System.out.println("Fallimento");
 		return null;
 	}
-	/* Metodo per duplicare la tabella in un secondo database
+
+	/*
+	 * Metodo per duplicare la tabella in un secondo database
+	 * 
 	 * @param nomeTabella: Nome della tabella con relativo database
+	 * 
 	 * @param libreria: Contiene i dati da duplicare
 	 */
 	public static void duplicazioneDati(String nomeTabella, ArrayList<Libro> libreria)
